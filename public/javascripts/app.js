ad907bdf20ab9fa50ad94f85e0fc2ba1f9a411c7
Adam Wilson <adamwilson1234@gmail.com>
2015-12-21 16:19:22 -0600

    pancake api done in class at 4:15pm. not working fully. probelm at app.js line 48'ish. auto is not defined error. also why all the xhr finished loading stuff?

---
+++ public/javascripts/app.js
@@ -0,0 +1,80 @@
+// namespace
+var app = app || {};
+var active = active || {};
+
+// model should be first
+app.Model = Backbone.Model.extend({
+});
+
+// define my 4 important parts!
+app.Collection = Backbone.Collection.extend({
+  model: app.Model, // what type of models will this collection hold?
+  url: '/api',
+  initialize: function() {
+    var self = this;
+    this.on('change', function() {
+      console.log('Our Collection changed.');
+      var view = new app.CollectionView({
+        collection: self
+      });
+    });
+    this.on('sync', function() {
+      console.log('Our Collection synced with the API.');
+      var view = new app.CollectionView({
+        collection: self
+      });
+    });
+    // get data from the API
+    this.fetch();
+  }
+});
+
+Backbone.Model.idAttribute = "_id";
+
+app.CollectionView = Backbone.View.extend({
+  el: $('#pancake-listing'),
+  initialize: function() {
+    console.log('CollectionView is a go.');
+    // when loaded, let us render immediately
+    this.render();
+  },
+  render: function() {
+    console.log('CollectionView is rendering.');
+    // we expect our CollectionView to be bound to a Collection
+    var models = this.collection.models;
+    for (var m in models) {
+      new app.ModelView({
+        model: models[m],
+        el: this.el
+      });
+    }
+  }
+});
+
+app.ModelView = Backbone.View.extend({
+  initialize: function() {
+    console.log('ModelView instantiated and awaiting orders, sir');
+    this.render();
+  },
+  render: function() {
+    console.log('ModelView rendering.');
+    var data = this.model.attributes;
+    console.log('grabbing template');
+    var template = $('#recipe-template').html();
+    console.log('transforming template');
+    var compileTpl = _.template(template);
+    console.log('creating html from template and model data');
+    var html = compileTpl(data);
+    console.log(html);
+    console.log('rendering to page..');
+    this.$el.append(html);
+  }
+});
+
+// mongoDB support!
+Backbone.Model.idAttribute = "_id";
+
+// the document is ready
+$(document).ready(function(){
+  active.collection = new app.Collection();
+});
